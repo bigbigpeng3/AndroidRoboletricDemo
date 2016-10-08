f3a710a801653ce2c246802361e27be8b89d1f90
张鹏 <zhangpeng@bailitop.com>
2016-10-08 11:57:08 +0800

    update for  some miss class

---
+++ app/src/test/java/com/me/www/androidroboletricdemo/MyExtendDemo/Children1.java
@@ -0,0 +1,15 @@
+package com.me.www.androidroboletricdemo.MyExtendDemo;
+
+/**
+ * Description :
+ * Email  : bigbigpeng3@gmail.com
+ * Author : peng zhang
+ * Date   : 2016-9-28
+ */
+
+public class Children1 extends Father {
+
+
+
+
+}
---
+++ app/src/test/java/com/me/www/androidroboletricdemo/MyExtendDemo/Father.java
@@ -0,0 +1,16 @@
+package com.me.www.androidroboletricdemo.MyExtendDemo;
+
+/**
+ * Description :
+ * Email  : bigbigpeng3@gmail.com
+ * Author : peng zhang
+ * Date   : 2016-9-28
+ */
+
+public class Father {
+
+    public void myClassName(){
+        System.out.println(this);
+    }
+
+}
